04fd8d0597005f904d0b4d003bf1dbaf1e79c5a1
Bulat Minnemullin <minnemullin.bulat@yandex.ru>
2018-03-17 13:14:49 +0300

    first

--- index.php
+++ index.php
@@ -8,29 +8,29 @@ $date = new class {
 	function getDate() { return date("d/m/Y h:i"); }
 };
 
-$app->get('hello/{name}', function ($name) use ($app) {
+$app->get('/hello/{name}', function ($name) use ($app) {
 	return 'Hello '.$app->escape($name);
 });
 	
-$app->get('public', function(){
+$app->get('/public', function(){
     header('Access-Control-Allow-Origin: *');
     header('Content-type: text/plain; charset=utf-8');
     header('Access-Control-Allow-Methods: GET,POST,DELETE');
   });
-$app->get('print', function(){
+$app->get('/print', function(){
     header('Content-type: text/plain; charset=utf-8');
     echo file_get_contents(basename(__FILE__));
   });
 
-$app->get('author', function(){
+$app->get('/author', function(){
     header('Content-type: text/html; charset=utf-8');
     echo '<h4>Bulat Minnemullin</h4>';
   });
-$app->get('info', function(){
+$app->get('/info', function(){
     phpinfo();
   });
 
-$app->get('', function(){
+$app->get('/', function(){
 	echo ($date -> getDate());
   });
   
