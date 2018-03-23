c22cdd3faae6ddab711053b1d7ff9a07548f97f3
Bulat Minnemullin <minnemullin.bulat@yandex.ru>
2018-03-23 15:08:51 +0300

    Added run

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $app->get('/public', function(){
   });*/
 $app->get('/print', function(){
     $headers = ('Content-type' =>'text/plain; charset=utf-8');
-    $text = __toString(file_get_contents(basename(__FILE__)));
+    $text = "". (file_get_contents(basename(__FILE__)));
 	return new Response ($text, 200, $headers);
   });
 
